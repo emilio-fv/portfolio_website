32bd0f3fa39c46f60f2f0822ceb8426c20b9b428
emilio-fv <emiliofv.sd@gmail.com>
2022-11-09 20:11:43 -0700

    Initial commit

---
+++ website/src/views/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Main = (props) => {
+    return (
+        <div>
+            {/* Navbar */}
+            {/* intro */}
+            {/* about me */}
+            {/* projects */}
+            {/* experience */}
+            {/* contact me */}
+        </div>
+    )
+}
+
+export default Main;
